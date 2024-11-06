55bf102b1bc3dc787e7ccb6d0c99837f1f0f97ed
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2024-11-06 22:46:11 +0530

    2.6 ongoing

--- Part 1/part2/phonebook/src/App.jsx
+++ Part 1/part2/phonebook/src/App.jsx
@@ -4,13 +4,23 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  let debugPersons = [];
+  for (let i = 0; i < persons.length; i++) {
+    debugPersons.push(persons[i].name);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <div>debug: {newName}</div>
+      <div>debug: {debugPersons}</div>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
