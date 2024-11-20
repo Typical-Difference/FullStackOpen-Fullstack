9ae3a466b90430d33e0e26f6b148cca72059d33f
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2024-11-20 21:47:00 +0530

    2.7 done

--- Part 1/part2/phonebook/src/App.jsx
+++ Part 1/part2/phonebook/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 const App = () => {
+  //persons is an array of objects with `name` field
+  //newName is used to pass a new value from the form
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
@@ -9,10 +11,15 @@ const App = () => {
   };
 
   const addPerson = (event) => {
-    event.preventDefault();
-    const newPersonObj = { name: newName };
-    const newPersonsArray = persons.concat(newPersonObj);
-    setPersons(newPersonsArray);
+    event.preventDefault(); //prevent default stops reload upon submitting for
+    const personsString = persons.map((person) => person.name);
+    if (personsString.includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPersonObj = { name: newName };
+      const newPersonsArray = persons.concat(newPersonObj);
+      setPersons(newPersonsArray);
+    }
   };
 
   let debugPersons = [];
@@ -27,16 +34,18 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNewName} />
         </div>
+        <div>debug:{newName}</div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
-        ))}
-      </ul>
+      {
+        //Traverse the persons array and display its contents
+        persons.map((person, index) => {
+          return <p key={index}>{person.name}</p>;
+        })
+      }
     </div>
   );
 };
