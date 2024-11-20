8b664ff9e516b09ce6758ecea3f0b55139ff6b50
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2024-11-20 22:48:34 +0530

    2.8 done

--- Part 1/part2/phonebook/src/App.jsx
+++ Part 1/part2/phonebook/src/App.jsx
@@ -1,31 +1,39 @@
 import { useState } from "react";
 
 const App = () => {
-  //persons is an array of objects with `name` field
+  //persons is an array of objects with `name` and `number` fields
   //newName is used to pass a new value from the form
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
   };
 
-  const addPerson = (event) => {
-    event.preventDefault(); //prevent default stops reload upon submitting for
-    const personsString = persons.map((person) => person.name);
-    if (personsString.includes(newName)) {
-      alert(`${newName} is already added to phonebook`);
-    } else {
-      const newPersonObj = { name: newName };
-      const newPersonsArray = persons.concat(newPersonObj);
-      setPersons(newPersonsArray);
-    }
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
-  let debugPersons = [];
-  for (let i = 0; i < persons.length; i++) {
-    debugPersons.push(persons[i].name);
-  }
+  const addPerson = (event) => {
+    event.preventDefault(); //prevent default stops reload upon submitting for
+    const personsNames = persons.map((person) => person.name);
+
+    //Check duplicates
+    if (personsNames.includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    //Add name and number
+    const newPersonObj = { name: newName, number: newNumber };
+    const newPersonsArray = persons.concat(newPersonObj);
+    setPersons(newPersonsArray);
+    setNewName("");
+    setNewNumber("");
+  };
 
   return (
     <div>
@@ -33,6 +41,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber}></input>
         </div>
         <div>debug:{newName}</div>
         <div>
@@ -43,7 +54,11 @@ const App = () => {
       {
         //Traverse the persons array and display its contents
         persons.map((person, index) => {
-          return <p key={index}>{person.name}</p>;
+          return (
+            <p key={index}>
+              {person.name} {person.number}
+            </p>
+          );
         })
       }
     </div>
