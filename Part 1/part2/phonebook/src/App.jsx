689078297e12f376e5ef2cee6a27d1285ff197a1
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2024-11-29 23:08:02 +0530

    2.10 done

--- Part 1/part2/phonebook/src/App.jsx
+++ Part 1/part2/phonebook/src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
 
-const App = () => {
-  //persons is an array of objects with `name` and `number` fields
-  //newName is used to pass a new value from the form
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+const Filter = ({ filterText, handleFilterText }) => {
+  return (
+    <div>
+      Filter shown with a{" "}
+      <input value={filterText} onChange={handleFilterText}></input>
+    </div>
+  );
+};
 
-  //States to handle input
+const PersonForm = ({ persons, setPersons }) => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [filterText, setFilterText] = useState("");
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
@@ -23,20 +21,12 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
-  const handleFilterText = (event) => {
-    setFilterText(event.target.value);
-  };
-
-  const filteredList = persons.filter((person) => {
-    return person.name.toLowerCase().includes(filterText);
-  });
-
   const addPerson = (event) => {
     event.preventDefault(); //prevent default stops reload upon submitting for
-    const personsNames = persons.map((person) => person.name);
+    const personsNames = persons.map((person) => person.name.toLowerCase());
 
     //Check duplicates
-    if (personsNames.includes(newName)) {
+    if (personsNames.includes(newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
@@ -51,12 +41,6 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <div>
-        Filter shown with a{" "}
-        <input value={filterText} onChange={handleFilterText}></input>
-      </div>
-      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -68,17 +52,56 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+    </div>
+  );
+};
+
+const Persons = ({ persons }) => {
+  //Traverse the persons array and display its contents
+  return (
+    <div>
+      {persons.map((person) => (
+        <Person key={person.name} person={person} />
+      ))}
+    </div>
+  );
+};
+
+const Person = ({ person }) => {
+  return (
+    <div>
+      {person.name} {person.number}
+    </div>
+  );
+};
+
+const App = () => {
+  //persons is an array of objects with `name` and `number` fields
+  //newName is used to pass a new value from the form
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
+  const [filterText, setFilterText] = useState("");
+
+  const handleFilterText = (event) => {
+    setFilterText(event.target.value.toLowerCase());
+  };
+  const filteredList = persons.filter((person) => {
+    return person.name.toLowerCase().includes(filterText);
+  });
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filterText={filterText} handleFilterText={handleFilterText} />
+      <h2>add a new</h2>
+      <PersonForm persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      {
-        //Traverse the persons array and display its contents
-        filteredList.map((item) => {
-          return (
-            <p key={item.id}>
-              {item.name} {item.number}
-            </p>
-          );
-        })
-      }
+      <Persons persons={filteredList} />
     </div>
   );
 };
